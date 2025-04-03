83d2f7735ff8ddb77584456099e52c15feccc92b
Rudra Narayan Mahapatro <mahapatro32@gmail.com>
2025-04-04 04:52:35 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -167,9 +167,6 @@ export default function Home() {
       </section>
 
       {/* Footer */}
-      <footer className="bg-white dark:bg-gray-800 py-6 text-center text-sm text-gray-500 dark:text-gray-400 border-t border-gray-200 dark:border-gray-700 mt-auto">
-        © 2025 Rudra Narayan Mohapatro. All rights reserved.
-      </footer>
     </div>
   );
 }
